7d0a972b71c93892cc6986c1082ae7605b2d06ba
TheRanger654 <han.reemer@gmail.com>
2018-02-02 22:50:24 +0100

    Alles
    Alles wat ik heb gedaan

---
+++ Assets/Scripts/CameraView.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraView : MonoBehaviour {
+
+    Camera c1;
+    public Camera c2;
+    Vector3 ypos;
+
+	// Use this for initialization
+	void Start () {
+        c1 = Camera.main;
+        c1.enabled = true;
+        c2.enabled = false;
+        ypos = c1.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(c1.transform.position);
+		if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (c1.enabled)
+            {
+                c2.enabled = true;
+                c1.enabled = false;
+
+            }
+            else if (!c1.enabled)
+            {
+                c1.enabled = true;
+                c2.enabled = false;
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public GameObject player;
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector3(0, 0, 5);
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = new Vector3(0, 0, -5);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector3(5, 0, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector3(-5, 0, 0);
+        }
+
+        else
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
